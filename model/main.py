8040b0e91dcd6a875e720cb7f679d6961a729036
karthikhosur <rheuzaki@gmail.com>
2021-06-16 13:55:38 +0530

    Add files via upload

---
+++ model/main.py
@@ -0,0 +1,202 @@
+import textract
+import re 
+from .name_parser import name_extractor
+from .email_id import email_id_extractor
+from .phone import phone_extraction
+from .skills import skills_extract
+from .personal_info import extract_address,extract_dob,extract_gender
+from .experience import extract_experience
+from .education import extract_education
+from .industry import industry_class
+import concurrent.futures
+
+
+
+
+result = {
+    "personal_info": [
+        {
+            "name": [
+                ""
+            ],
+            "phone": [
+                ""
+            ],
+            "isd_code": [
+                ""
+            ],
+            "email": [
+                ""
+            ],
+            "url": [
+                ""
+            ],
+            "address": [
+                {
+                    "postal_code": " ",
+                    "place_name": "",
+                    "state_name": "",
+                    "city_name": "",
+                    "area_name": " ",
+                    "longitude": " ",
+                    "latitude": ""
+                }
+            ],
+            "passport_no": "",
+            "dob": "",
+            "gender": ""
+        }
+    ],
+    "education": {
+        "edu_text": "",
+        "edu_history": [
+            {
+                "id": "",
+                "degree": "",
+                "university": "",
+                "grade": "",
+                "graduation_year": ""
+            }
+        ]
+    },
+    "experience": {
+        "exp_text": "",
+        "exp_history": [
+            {
+                "id": "",
+                "organization": "",
+                "job_designation": "",
+                "start_date": "",
+                "end_date": ""
+            }
+        ],
+        "workex_skills": []
+    },
+    "total_experience": "0",
+    "priority_skills": [
+        []
+    ],
+    "all_skills": [
+        []
+    ],
+    "other_sections": [
+        []
+    ],
+    "text": [
+        ""
+    ],
+    "industry":""
+}
+
+
+def main(file_name,file_type):
+    # try:
+        s = textract.process(file_name)
+        text =str(s, 'utf-8', 'ignore')
+        skills_list = []
+
+        text = re.sub("\\xad|\\u200b|\\t|\t"," ", text)
+        temp_terms=[]
+        exclude_re =  "Bio-Data|BIODATA|R E S U M E|Bio data|P a g e|biodata|Page|PAGE|page|CURRICULUMVITAE|CURRICULAM VITAE|CURRICULUM VITAE|●|Curriculum Vitae|CV|Resume|RESUME|CONFIDENTIAL|Look forward|My CV is detailed below|Work is Worship|Not keen on sales" + "|DSP MERRILYNCH|Private & Confidential|(Private and Confidential)" +"|Private and Confidential|Please feel free to contact|Moved to" +"|a manager who is……..|Here is the resume of the applicant|trial version can convert" +"|(this message is omitted in registered version)|converted by activertf trial version" +"|planman consulting|referred|reffered|Last active|Last Modified|NOT LOOKING FOR CHANGE" +"|Timesjobs profile|Jobstreet profile|Monster profile|Naukri Profile" +"|click here to view resume in doc format|click here to unsubscribe" +"|Add Comments to Resume|BIO -  DATA|Consultants|Names will be provided on request|circulam vitae|urriculam vitae" +"|Qualification|Total Exp|Skill sets|CIRRUCULAM VITAE|C  U  R  R  I  C  U  L  U  M      V  I  T  A  E|present company" + "|total exp|circulam vitae|urriculam vitae|SAP-|Ref. By|CIRCULAM - VITAE|CIRCULAM-VITAE|job code" + "|i am writing|my objective is|i am confident|currently i am|i am enclosing|my norm is" +"|BIO - DATA|.Curriculum  Vitae|Carriculam Vitae|CIRRICULAM VITAE|CARRICULUM VITAE|urriculum Vitae|CRRICULUM VITAE|CUURICLUM VITAE|urriculum Vitae|- CIRCULLUM VITAE -|DCURRICULAM|CARICULAM VITAE" +"|RICULUM VITAE|CARRICULAM VITAE|Initial Contact - Curriculum Vitae|CIRCULUM VITAE|CIRRICULUM VITAE|comprehensive|URRICULUM  VITAE|CURRICULUM  VITAE|IRCULUM VITAE" +"|CURICURUM VITAE|CIRICULAM-REVITA|RESUMẾ|Om Sai Ram|A        R        C        H        I        T        E        C        T|Contact Address :" +"|Age :|Contact No. :|Email Id :|Nationality/Sex :|Languages known :" +"|Associate|CARRICULAM VITE|CERTIFIED|POST APPLIED|Gender|Nationality|Contact Address|MICROSOFT CERTIFIED|rRESUME|CONCISE RESUME|View Resume|(Word 2000 Format)|View Text Resume Only|Forward This Resume|Contact by Email|Print Resume|Save to Folder|Close Window|Go to:|E.CTC:|C.CTC:"
+
+        text =re.sub(exclude_re," ",text)
+        terms = text.splitlines()
+        temp= ""
+
+        count_split_words =0
+
+        for i in range(len(terms)):
+            if re.search("\s[a-zA-Z]{1}\s",terms[i]) :
+                count_split_words +=1
+
+                temp= re.sub("\s","",terms[i])
+                if len(temp)< 18:
+                    temp_terms.append(temp)
+                else:
+                    temp_terms.append(terms[i])
+            else:
+                temp_terms.append(terms[i])
+
+        terms =temp_terms
+
+
+        for i in range(len(terms)):
+            text = text +" "+ terms[i]
+
+
+
+
+        # if count_split_words>14 and not re.search("doc|docx",file_type):
+        #     s2 = textract.process(file_name, method='tesseract')
+        #     text2 = str(s2, 'utf-8', 'ignore')
+        #     text2 =re.sub(exclude_re," ",text2)
+
+        #     text2 = re.sub("\\xad|\\u200b|\\t"," ", text2)
+        #     terms2 = text2.splitlines()
+
+        #     with concurrent.futures.ThreadPoolExecutor() as executor:
+        #         future1 = executor.submit(email_id_extractor, terms2,text2)
+        #         future2 = executor.submit(phone_extraction, terms2,text2)
+        #         future3 = executor.submit(skills_extract,text2)
+        #         future4 = executor.submit(extract_address,text2)
+        #         future5 = executor.submit(extract_experience,terms2)
+        #         future6 = executor.submit(extract_education,terms2)
+        #         future7 = executor.submit(industry_class,text2)
+        #         future8 = executor.submit(extract_dob,text2)
+        #         future9 = executor.submit(extract_gender,text2)
+        #         email_id  = future1.result()
+        #         isd_code, phone_no = future2.result()
+        #         skills_list = future3.result()
+        #         address,country_code= future4.result()
+        #         exp_result,exp_dur= future5.result()
+        #         edu_result= future6.result()
+        #         industry= future7.result()
+        #         dob = future8.result()
+        #         gender= future9.result()
+
+        # else:
+        if 1 :
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                future1 = executor.submit(email_id_extractor, terms,text)
+                future2 = executor.submit(phone_extraction, terms,text)
+                future3 = executor.submit(skills_extract,text)
+                future4 = executor.submit(extract_address,text)
+                future5 = executor.submit(extract_experience,terms)
+                future6 = executor.submit(extract_education,terms)
+                future7 = executor.submit(industry_class,text)
+                future8 = executor.submit(extract_dob,text)
+                future9 = executor.submit(extract_gender,text)
+                email_id  = future1.result()
+                isd_code, phone_no = future2.result()
+                skills_list = future3.result()
+                address,country_code= future4.result()
+                exp_result,exp_dur= future5.result()
+                edu_result= future6.result()
+                industry= future7.result()
+                dob = future8.result()
+                gender= future9.result()
+
+
+        person_name = name_extractor(text,terms,email_id,file_type)
+
+
+        result["personal_info"][0]["name"][0] =person_name
+        result["personal_info"][0]["phone"][0] = phone_no
+        result["personal_info"][0]["isd_code"][0] = isd_code
+        result["personal_info"][0]["email"][0] = email_id
+        result["personal_info"][0]["address"][0] = address
+        result["personal_info"][0]["dob"] = dob
+        result["personal_info"][0]["gender"] = gender
+        result["education"] = edu_result
+        result["experience"] = exp_result
+
+        result["total_experience"] = exp_dur
+        result["all_skills"][0] = skills_list
+
+        result["text"][0] = text
+        result["industry"] = industry
+
+        return result
+
+    # except:
+    #     return result
---
+++ model/personal_info.py
@@ -0,0 +1,117 @@
+import re
+import os
+
+
+city_names_india = [" abohar "," achalpur "," adilabad "," adityapur "," adoni "," adrymbai "," agartala "," agra "," ahmadabad "," ahmadnagar "," ahmed nagar "," ahmedabad "," aizawl "," ajmer "," akbarpur "," akola "," alandur "," alappuzha "," aligarh "," alirajpur "," allahabad "," almora "," alwar "," ambala "," ambala sadar "," ambarnath "," ambattur "," ambedkar nagar "," ambikapur "," ambur "," amravati "," amreli "," amritsar "," amroha "," anand "," anantapur "," ananthapur "," ananthnag "," anantnag "," angul "," anuppur "," araria "," ariyalur "," arlu "," arrah "," arwal "," asansol "," ashok nagar "," ashoknagar kalyangarh "," auraiya "," aurangabad "," aurangabad(bh) "," avadi "," azamgarh "," badlapur "," bagaha "," bagalkot "," bageshwar "," bagpat "," bahadurgarh "," baharampur "," bahraich "," baidyabati "," balaghat "," balangir "," baleshwar town "," baleswar "," ballia "," bally "," bally city "," balrampur "," balurghat "," banaskantha "," banda "," bandipur "," bangalore "," bangalore rural "," banka "," bankura "," bansberia "," banswara "," barabanki "," baramulla "," baran "," baranagar "," barasat "," baraut "," barddhaman "," bardhaman "," bareilly "," bargarh "," baripada town "," barmer "," barnala "," barpeta "," barrackpur "," barshi "," barwani "," basirhat "," bastar "," basti "," batala "," bathinda "," beawar "," beed "," begusarai "," belgaum "," bellary "," bengaluru "," bettiah "," betul "," bhadrak "," bhadravati "," bhadreswar "," bhagalpur "," bhalswa jahangir pur "," bhandara "," bharatpur "," bharuch "," bhatpara "," bhavnagar "," bhilai nagar "," bhilwara "," bhimavaram "," bhind "," bhiwadi "," bhiwandi "," bhiwani "," bhojpur "," bhopal "," bhubaneswar "," bhuj "," bhusawal "," bid "," bidar "," bidhan nagar "," biharsharif "," bijapur "," bijapur(cgh) "," bijapur(kar) "," bijnor "," bikaner "," bilaspur "," bilaspur (hp) "," bilaspur(cgh) "," birbhum "," bishnupur "," bokaro "," bokaro steel city "," bombay "," bongaigaon "," bongaon "," botad "," boudh "," brahmapur town "," budaun "," budgam "," bulandshahr "," buldhana "," bundi "," burari "," burhanpur "," buxar "," cachar "," central delhi "," chamba "," chamoli "," champawat "," champdani "," champhai "," chamrajnagar "," chandannagar "," chandauli "," chandausi "," chandel "," chandigarh "," chandrapur "," changlang "," chapra "," chas "," chatra "," chennai "," chhatarpur "," chhattarpur "," chhindwara "," chickmagalur "," chikkaballapur "," chikmagalur "," chilakaluripet "," chitradurga "," chitrakoot "," chittaurgarh "," chittoor "," chittorgarh "," churachandpur "," churu "," coimbatore "," cooch behar "," cuddalore "," cuddapah "," cuttack "," dabgram "," dadra & nagar haveli "," dahod "," dakshina kannada "," dallo pura "," daman "," damoh "," dantewada "," darbhanga "," darjiling "," darrang "," datia "," dausa "," davanagere "," davangere "," debagarh "," deesa "," dehradun "," dehri "," delhi "," delhi cantonment "," deoghar "," deoli "," deoria "," dewas "," dhalai "," dhamtari "," dhanbad "," dhar "," dharmapuri "," dharmavaram "," dharwad "," dhaulpur "," dhemaji "," dhenkanal "," dholpur "," dhubri "," dhule "," dibang valley "," dibrugarh "," dimapur "," dinapur nizamat "," dindigul "," dindori "," diu "," doda "," dum dum "," dumka "," dungarpur "," durg "," durgapur "," east champaran "," east delhi "," east garo hills "," east godavari "," east kameng "," east khasi hills "," east midnapore "," east nimar "," east siang "," east sikkim "," east singhbhum "," eluru "," english bazar "," ernakulam "," erode "," etah "," etawah "," faizabad "," faridabad "," faridkot "," farrukhabad "," farrukhabad-cum-fatehgarh "," fatehabad "," fatehgarh sahib "," fatehpur "," fazilka "," firozabad "," firozpur "," gadag "," gadag-betigeri "," gadchiroli "," gajapati "," gandhi nagar "," gandhidham "," gandhinagar "," ganganagar "," gangapur city "," gangawati "," ganj "," ganjam "," garhwa "," gariaband "," gautam buddha nagar "," gaya "," ghaziabad "," ghazipur "," giridh "," giridih "," goalpara "," godda "," godhra "," gokal pur "," golaghat "," gonda "," gondal "," goas "," gondia "," gondiya "," gopalganj "," gorakhpur "," greater hyderabad "," greater mumbai "," greater noida "," gudivada "," gulbarga "," gumla "," guna "," guntakal "," guntur "," gurdaspur "," gurgaon "," gurugram "," guwahati "," gwalior "," habra "," hailakandi "," hajipur "," haldia "," haldwani-cum-kathgodam "," halisahar "," hamirpur "," hamirpur(hp) "," hanumangarh "," haora "," hapur "," harda "," hardoi "," hardwar "," haridwar "," hassan "," hastsal "," hathras "," haveri "," hazaribag "," hindaun "," hindupur "," hinganghat "," hingoli "," hisar "," hooghly "," hoshangabad "," hoshiarpur "," hospet "," hosur "," howrah "," hubli-dharwad "," hugli-chinsurah "," hyderabad "," ichalkaranji "," idukki "," imphal "," imphal east "," imphal west "," indore "," jabalpur "," jagadhri "," jagatsinghapur "," jagdalpur "," jaintia hills "," jaipur "," jaisalmer "," jajapur "," jalandhar "," jalaun "," jalgaon "," jalna "," jalor "," jalpaiguri "," jamalpur "," jammu "," jamnagar "," jamshedpur "," jamtara "," jamui "," jamuria "," janjgir-champa "," jashpur "," jaunpur "," jehanabad "," jetpur navagadh "," jhabua "," jhajjar "," jhalawar "," jhansi "," jharsuguda "," jhujhunu "," jhunjhunun "," jind "," jodhpur "," jorhat "," junagadh "," jyotiba phule nagar "," k.v.rangareddy "," kachchh "," kadapa "," kaimur (bhabua) "," kaithal "," kakinada "," kalahandi "," kalol "," kalyani "," kamarhati "," kamrup "," kancheepuram "," kanchipuram "," kanchrapara "," kandhamal "," kangra "," kanker "," kannauj "," kannur "," kanpur "," kanpur city "," kanpur dehat "," kanpur nagar "," kanyakumari "," kapurthala "," karaikal "," karaikkudi "," karauli "," karawal nagar "," karbi anglong "," kargil "," karim nagar "," karimganj "," karimnagar "," karnal "," karur "," kasargod "," kasganj "," kashipur "," kathua "," katihar "," katni "," kaushambi "," kawardha "," kendrapara "," kendujhar "," khagaria "," khammam "," khandwa "," khanna "," kharagpur "," khardaha "," khargone "," kheda "," kheri "," khora "," khorda "," khunti "," khurja "," kinnaur "," kiphire "," kirari suleman nagar "," kishanganj "," kishangarh "," kochi "," kodagu "," koderma "," kohima "," kokrajhar "," kolar "," kolasib "," kolhapur "," kolkata "," kollam "," koppal "," koraput "," korba "," koriya "," kota "," kottayam "," kozhikode "," krishna "," krishnagiri "," krishnanagar "," kulgam "," kullu "," kulti "," kumbakonam "," kupwara "," kurichi "," kurnool "," kurukshetra "," kurung kumey "," kushinagar "," lahul & spiti "," lakhimpur "," lakhisarai "," lakshadweep "," lalitpur "," latehar "," latur "," lawngtlai "," leh "," lohardaga "," lohit "," longleng "," loni "," lower subansiri "," lucknow "," ludhiana "," lunglei "," machilipatnam "," madanapalle "," madavaram "," madhepura "," madhubani "," madhyamgram "," madurai "," mahabub nagar "," maharajganj "," mahasamund "," mahbubnagar "," mahe "," mahendragarh "," mahesana "," maheshtala "," mahoba "," mainpuri "," malappuram "," malda "," malegaon "," malerkotla "," malkangiri "," mammit "," mandi "," mandla "," mandoli "," mandsaur "," mandya "," mangalore "," mango "," mansa "," marigaon "," mathura "," maunath bhanjan "," mayurbhanj "," medak "," medinipur "," meerut "," mira bhayander "," miryalaguda "," mirzapur "," mirzapur-cum-vindhyachal "," modinagar "," moga "," mohali "," mokokchung "," mon "," moradabad "," morena "," morvi "," motihari "," mughalsarai "," muktsar "," mumbai "," munger "," murshidabad "," murwara "," mustafabad "," muzaffarnagar "," muzaffarpur "," mysore "," nabadwip "," nabarangapur "," nadia "," nadiad "," nagaon "," nagapattinam "," nagaur "," nagda "," nagercoil "," nagpur "," naihati "," nainital "," nalanda "," nalbari "," nalgonda "," namakkal "," nanded "," nanded waghala "," nandurbar "," nandyal "," nangloi jat "," narasaraopet "," narayanpur "," narmada "," narsinghpur "," nashik "," navi mumbai "," navsari "," nawada "," nawanshahr "," nayagarh "," neemuch "," nellore "," new delhi "," neyveli "," nicobar "," nilgiris "," nizamabad "," noida "," north 24 parganas "," north and middle andaman "," north barrackpur "," north cachar hills "," north delhi "," north dinajpur "," north dum dum "," north goa "," north sikkim "," north tripura "," north west delhi "," nuapada "," ongole "," orai "," osmanabad "," ozhukarai "," pakur "," palakkad "," palamau "," palanpur "," pali "," pallavaram "," palwal "," panch mahals "," panchkula "," panihati "," panipat "," panna "," panvel "," papum pare "," parbhani "," patan "," pathanamthitta "," pathankot "," patiala "," patna "," pauri garhwal "," perambalur "," peren "," phek "," pilibhit "," pimpri chinchwad "," pithampur "," pithoragarh "," pondicherry "," poonch "," porbandar "," port blair "," prakasam "," pratapgarh "," proddatur "," puducherry "," pudukkottai "," pulwama "," pune "," puri "," purnia "," puruliya "," rae bareli "," raebareli "," raichur "," raiganj "," raigarh "," raigarh(mh) "," raipur "," raisen "," rajahmundry "," rajapalayam "," rajarhat gopalpur "," rajauri "," rajgarh "," rajkot "," rajnandgaon "," rajpur sonarpur "," rajsamand "," ramagundam "," ramanagar "," ramanathapuram "," ramgarh "," rampur "," ranchi "," ranibennur "," raniganj "," ratlam "," ratnagiri "," raurkela industrial township "," raurkela town "," rayagada "," reasi "," rewa "," rewari "," ri bhoi "," rishra "," robertson pet "," rohtak "," rohtas "," roorkee "," ropar "," rudraprayag "," rudrapur "," rupnagar "," s.a.s. nagar "," sabarkantha "," sagar "," saharanpur "," saharsa "," sahibganj "," saiha "," salem "," samastipur "," sambalpur "," sambhal "," sangli "," sangli miraj kupwad "," sangrur "," sant kabir nagar "," sant ravidas nagar "," santipur "," saran "," sasaram "," satara "," satna "," sawai madhopur "," secunderabad "," sehore "," senapati "," seoni "," seraikela-kharsawan "," serampore "," serchhip "," shahdol "," shahjahanpur "," shajapur "," shamli "," sheikhpura "," sheohar "," sheopur "," shikohabad "," shillong "," shimla "," shimoga "," shivpuri "," shrawasti "," sibsagar "," siddharthnagar "," sidhi "," sikar "," silchar "," siliguri "," simdega "," sindhudurg "," singrauli "," sirmaur "," sirohi "," sirsa "," sitamarhi "," sitapur "," sivaganga "," siwan "," solan "," solapur "," sonapur "," sonbhadra "," sonipat "," sonitpur "," south 24 parganas "," south andaman "," south delhi "," south dinajpur "," south dum dum "," south garo hills "," south goa "," south sikkim "," south tripura "," south west delhi "," srikakulam "," srinagar "," sujangarh "," sultan pur majra "," sultanpur "," sundergarh "," supaul "," surat "," surendra nagar "," surendranagar dudhrej "," surguja "," suryapet "," tadepalligudem "," tadpatri "," tambaram "," tamenglong "," tapi "," tarn taran "," tawang "," tehri garhwal "," tenali "," thane "," thanesar "," thanjavur "," the dangs "," theni "," thiruvananthapuram "," thoothukkudi "," thoubal "," thrissur "," tikamgarh "," tinsukia "," tirap "," tiruchirappalli "," tirunelveli "," tirupati "," tiruppur "," tiruvallur "," tiruvannamalai "," tiruvarur "," tiruvottiyur "," titagarh "," tonk "," tuensang "," tumkur "," tuticorin "," udaipur "," udgir "," udham singh nagar "," udhampur "," udupi "," ujjain "," ukhrul "," ulhasnagar "," uluberia "," umaria "," una "," unnao "," upper siang "," upper subansiri "," urwati "," uttara kannada "," uttarkashi "," uttarpara kotrung "," vadodara "," vaishali "," valsad "," varanasi "," vasai virar city "," vellore "," veraval "," vidisha "," vijayawada "," villupuram "," virudhunagar "," visakhapatnam "," vizianagaram "," warangal "," wardha "," washim "," wayanad "," west champaran "," west delhi "," west garo hills "," west godavari "," west kameng "," west khasi hills "," west midnapore "," west nimar "," west siang "," west sikkim "," west singhbhum "," west tripura "," wokha "," yadgir "," yamuna nagar "," yamunanagar "," yavatmal "," zunhebotto "]
+
+
+us_states_short = ["AL","AZ","AR","CA","CO","CT","DE","FL","GA","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"]
+
+us_states_full = ["alabama","alaska","arizona","arkansas","california","colorado","connecticut","delaware","district of columbia","florida","georgia","hawaii","idaho","illinois","indiana","iowa","kansas","kentucky","louisiana","maine","montana","nebraska","nevada","new hampshire","new jersey","new mexico","new york","north carolina","north dakota","ohio","oklahoma","oregon","maryland","massachusetts","michigan","minnesota","mississippi","missouri","pennsylvania","rhode island","south carolina","south dakota","tennessee","texas","utah","vermont","virginia","washington","west virginia","wisconsin","wyoming"]
+
+
+personal_info_headings= ["personal"]
+
+
+def extract_address(text):
+    try:
+        # india code =0 and us code =1 
+        country_code = 1 
+        address={"postal_code":" ","place_name":" ","state_name":" ","city_name":" ","area_name":" ","longitude":" ","latitude":" "}
+        pincode = ""
+        ind_pincode = ""
+        indian_city= ""
+        us_pincode =""
+        us_city= ""
+        state_name =""
+
+
+        text = re.sub("\.|\-|,"," ",text)
+
+
+        # Indian Address
+
+        #Search for Indian Pincode
+        if re.search(r"\b\d{3}\s{0,1}\d{3}\b",text):
+            ind_pincode =str(re.search(r"\b\d{3}\s{0,1}\d{3}\b",text)[0])
+            if not int(ind_pincode[0])>0 or not int(ind_pincode[0])<9:
+                ind_pincode = ""
+            
+
+        if ind_pincode != "":
+            for i in range(len(city_names_india)):
+                if re.search(city_names_india[i],text.lower()) :
+                    if len(re.search(city_names_india[i],text.lower())[0])>4:
+                        indian_city = re.search(city_names_india[i],text.lower())[0]
+                        break
+        
+        if indian_city == "" and  ind_pincode == "":
+            if re.search(r"\b\d{5}([-+]?\d{4})?\b",text):
+                us_pincode=str(re.search(r"\b\d{5}([-+]?\d{4})?\b",text)[0])
+            if not int(us_pincode[0])>0 or not int(us_pincode[0])<9:
+                us_pincode =""
+
+            if re.search(r"\b\d{5}\b",text):
+                us_pincode = re.search(r"\b\d{5}\b",text)[0]
+
+            for i in range(len(us_states_short)):
+                if re.search(us_states_short[i],text):
+                    state_name = us_states_full[i]
+
+            if state_name == "":
+
+                for i in range(len(us_states_full)):
+                    if re.search(us_states_full[i],text.lower()):
+                        state_name = us_states_full[i]
+
+        if ind_pincode != "":
+            country_code = 0
+            address["postal_code"] = ind_pincode
+        if us_pincode != "":
+            country_code=1
+            address["postal_code"] = us_pincode
+        if indian_city != "":
+            country_code =0
+            address["place_name"] = indian_city
+            address["city_name"] = indian_city
+        if state_name != "":
+            country_code=1
+            address["place_name"] = state_name
+
+            address["city_name"] = state_name
+        return address,country_code
+    except:
+
+        return address,country_code
+
+def extract_dob(text):
+    try:
+        birth_date = ""
+        match_dob=""
+        dob_words = "Born on|Date of birth|DOB :|DOB|DOB:|DATE OF BIRTH|Birth Date|Birth :|D.O.B|d. o. b|d o b|d  o  b|date and place of birth:|date and place of birth|date and country of birth|dateofbirth|data of birth|date of  birth|birthdate|date of birth/age:|date of birth/age|date of birthage|b\\'date|b’date|date  of  birth|date of birth|date ofbirth|dob|date & place of birth|d.o.b|date of birth|date-of-birth|date   of   birth|BORN:"
+        date_text = ""
+        if re.search(dob_words,text):
+            date_end = re.search(dob_words,text).end() 
+            date_text = text[date_end:date_end+20]
+            print("date : ", date_text)
+            if re.search("\d{4}",date_text):
+                date_end = re.search("\d{4}",date_text).end()
+                date_text =date_text[:date_end]
+            if not re.search("\d{4}|\d{2}",date_text):
+                date_text= ""
+        return date_text
+    except:
+        return ""
+
+def extract_gender(text):
+    try:
+        gender =""
+        re_gender = "Male|MALE|FEMALE|Female|female|male"
+
+        if re.search(re_gender,text):
+            gender =re.search(re_gender,text)[0]
+
+        return gender
+    except:
+        return ""
+
